90bce7a1019eb63a724f910e33a5a631a82f13d8
varungujarathi9 <v.gujarathi777@gmail.com>
2024-01-27 15:57:01 -0800

    update script

--- update-packages.sh
+++ update-packages.sh
@@ -1,5 +1,10 @@
+echo "---------------APT UPDATE---------------\n"
 sudo apt update -y
+echo "---------------APT FULL-UPGRADE---------------\n"
 sudo apt full-upgrade -y
+echo "---------------SNAP REFRESH---------------\n"
 sudo snap refresh
+echo "---------------APT AUTOREMOVE---------------\n"
 sudo apt autoremove -y
+echo "---------------FLATPAK UPDATE---------------\n"
 sudo flatpak update -y
